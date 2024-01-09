58587692aaaa0dcb4c145111df578641c463da67
Mohammad Hossein Bamorovat Abadi <m.bamorovvat@gmail.com>
2024-01-09 17:54:34 +0000

    add requirements.txt Edit the train.py

--- train.py
+++ train.py
@@ -3,14 +3,13 @@ import time
 import numpy as np
 import torch
 import glob
-from configs.config_F import params
+from config import params
 from torch import nn, optim
 from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
-from dataset_preparation import VideoDataset
+from dataloader import VideoDataset
 from models import C3D_model, R2Plus1D_model, R3D_model, Slow_Fast_model
 from tensorboardX import SummaryWriter
-import neptune.new as neptune
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
 import pandas as pd
@@ -18,13 +17,6 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from tqdm import tqdm
 
-run = neptune.init(
-    name="one_C3D",
-    # source_files=["train_new.py", "config_O.py", "dataset_preparation.py", "models/C3D_model.py"],
-    description='Model:C3D, View: Front, situation:Normall',
-    project="m.bamorovat/HAR-One-newframes",
-    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlMzk0NzVhZi05MmJmLTQzZWEtYjU2Ni01YjUwMzA4MmZkN2MifQ==",
-)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
@@ -33,6 +25,7 @@ print("Device being used:", device)
 y_pred = []
 y_true = []
 best_test_acc1 = 0.0
+
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -91,7 +84,6 @@ def train(model, train_dataloader, epoch, criterion, optimizer, writer):
 
     train_loop = tqdm(enumerate(train_dataloader), total = len(train_dataloader), leave=False)
 
-    #for step, (inputs1, labels) in enumerate(train_dataloader):
     for step, (inputs1,labels) in train_loop:
         data_time.update(time.time() - end)
 
@@ -115,10 +107,8 @@ def train(model, train_dataloader, epoch, criterion, optimizer, writer):
         train_loop.set_description(f"Epoch [{epoch}/{params['epoch_num']}]")
         train_loop.set_postfix(loss=losses.avg, top1=top1.avg, top5=top5.avg)
 
-        #if (step + 1) % params['display'] == 0:
     print('---------------------- Train ---------------------------------')
-            # for param in optimizer.param_groups:
-            #     print('lr: ', param['lr'])
+
     print_string = 'Epoch: [{0}][{1}/{2}]'.format(epoch, step + 1, len(train_dataloader))
     print(print_string)
     print_string = 'data_time: {data_time:.3f}, batch time: {batch_time:.3f}'.format(
@@ -133,10 +123,6 @@ def train(model, train_dataloader, epoch, criterion, optimizer, writer):
     writer.add_scalar('train_loss_epoch', losses.avg, epoch)
     writer.add_scalar('train_top1_acc_epoch', top1.avg, epoch)
     writer.add_scalar('train_top5_acc_epoch', top5.avg, epoch)
-
-    run["train_loss_epoch"].log(losses.avg, epoch)
-    run["train_top1_acc_epoch"].log(top1.avg, epoch)
-    run["train_top5_acc_epoch"].log(top5.avg, epoch)
 
 
 def validation(model, val_dataloader, epoch, criterion, optimizer, writer):
@@ -155,7 +141,6 @@ def validation(model, val_dataloader, epoch, criterion, optimizer, writer):
 
     with torch.no_grad():
         val_loop = tqdm(enumerate(val_dataloader), total = len(val_dataloader), leave=False)
-        #for step, (inputs1, labels) in enumerate(val_dataloader):
         for step, (inputs1, labels) in val_loop:
             data_time.update(time.time() - end)
             inputs1 = inputs1.to(device)
@@ -171,7 +156,6 @@ def validation(model, val_dataloader, epoch, criterion, optimizer, writer):
             top5.update(prec5.item(), inputs1.size(0))
             batch_time.update(time.time() - end)
             end = time.time()
-            #if (step + 1) % params['display'] == 0:
             val_loop.set_description(f"Epoch [{epoch}/{params['epoch_num']}]")
             val_loop.set_postfix(loss=losses.avg, top1=top1.avg, top5=top5.avg)
 
@@ -190,10 +174,6 @@ def validation(model, val_dataloader, epoch, criterion, optimizer, writer):
     writer.add_scalar('val_loss_epoch', losses.avg, epoch)
     writer.add_scalar('val_top1_acc_epoch', top1.avg, epoch)
     writer.add_scalar('val_top5_acc_epoch', top5.avg, epoch)
-
-    run["val_loss_epoch"].log(losses.avg, epoch)
-    run["val_top1_acc_epoch"].log(top1.avg, epoch)
-    run["val_top5_acc_epoch"].log(top5.avg, epoch)
 
     return top1.avg
 
@@ -214,7 +194,6 @@ def test(model, test_dataloader, epoch, criterion, optimizer, writer, better):
 
     with torch.no_grad():
         test_loop = tqdm(enumerate(test_dataloader), total = len(test_dataloader), leave=False)
-        #for step, (inputs1, labels) in enumerate(test_dataloader):
         for step, (inputs1, labels) in test_loop:
             data_time.update(time.time() - end)
             inputs1 = inputs1.to(device)
@@ -239,7 +218,6 @@ def test(model, test_dataloader, epoch, criterion, optimizer, writer, better):
             test_loop.set_postfix(loss=losses.avg, top1=top1.avg, top5=top5.avg)
 
     top1_acc = top1.avg
-           # if (step + 1) % params['display'] == 0:
     print('---- End Test ----')
     print_string = 'Epoch: [{0}][{1}/{2}]'.format(epoch, step + 1, len(test_dataloader))
     print(print_string)
@@ -256,14 +234,9 @@ def test(model, test_dataloader, epoch, criterion, optimizer, writer, better):
     writer.add_scalar('test_top1_acc_epoch', top1.avg, epoch)
     writer.add_scalar('test_top5_acc_epoch', top5.avg, epoch)
 
-    run["test_loss_epoch"].log(losses.avg, epoch)
-    run["test_top1_acc_epoch"].log(top1.avg, epoch)
-    run["test_top5_acc_epoch"].log(top5.avg, epoch)
-    
     global best_test_acc1
-    # best = max(top1_acc, best_test_acc1)
+
     if top1_acc > best_test_acc1:
-    # if better:
         # constant for classes
         classes = ['Bending', 'SittingDown', 'ClosingCan', 'Reaching', 'Walking', 'Drinking', 'StairsClimbingUp',
                'StairsClimbingDown', 'StandingUp', 'OpeningCan', 'CarryingObject', 'Cleaning', 'PuttingDownObjects',
@@ -279,12 +252,8 @@ def test(model, test_dataloader, epoch, criterion, optimizer, writer, better):
         save_name = '/home/mb19aag/test/RH_HAR_Oneview/output' + 'confM' + '-' + params['view1'] + params['situation'] + '.' + 'png'
         plt.savefig(save_name)
         
-        run['model/confusion'] = neptune.types.File(save_name)
-        run["Best_test_loss_epoch"].log(losses.avg, epoch)
-        run["Best_test_top1_acc_epoch"].log(top1.avg, epoch)
-        run["Best_test_top5_acc_epoch"].log(top5.avg, epoch)
-        
     best_test_acc1 = max(top1_acc, best_test_acc1)
+
 
 def main():
     val_acc = 0.0
@@ -345,7 +314,7 @@ def main():
         raise NotImplementedError
 
     model = model.to(device)
-    model = nn.DataParallel(model, device_ids=params['gpu'])  # multi-Gpu
+    model = nn.DataParallel(model, device_ids=params['gpu'])
 
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.SGD(train_params, lr=params['learning_rate'], momentum=params['momentum'],
@@ -353,7 +322,6 @@ def main():
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=params['step'], gamma=0.1)
 
     print('Total params: %.2fM' % (sum(p.numel() for p in model.parameters()) / 1000000.0))
-    run["parameters"] = params
 
     best_acc1 = 0.0
     for epoch in range(params['epoch_num']):
@@ -368,27 +336,11 @@ def main():
             # remember best acc@1 and save checkpoint
             is_best = acc1 > best_acc1
             best_acc1 = max(acc1, best_acc1)
-            # if epoch % save_epoch == (save_epoch - 1):
         better = False
-        # if is_best:
-            # better = True
-            #torch.save({
-            #    'epoch': epoch + 1,
-            #    'best_acc1': best_acc1,
-            #    'state_dict': model.state_dict(),
-            #    'opt_dict': optimizer.state_dict(),
-            #}, os.path.join(save_dir, 'models', saveName + '.pth.tar'))
-            #print("Save model at {}\n".format(os.path.join(save_dir, 'models', saveName + '.pth.tar')))
-            # print("Saved Epoch Number is: ", epoch)
 
-            #test(model, test_dataloader, epoch, criterion, optimizer, writer, better)
-
-        #if useTest and epoch % params['nTestInterval'] == (params['nTestInterval'] - 1):
         test(model, test_dataloader, epoch, criterion, optimizer, writer, better)
 
-    writer.close
-    run['model/best'] = neptune.types.File(os.path.join(save_dir, 'models', saveName + '.pth.tar'))
-    run.stop()
+    writer.close()
 
 
 if __name__ == '__main__':
